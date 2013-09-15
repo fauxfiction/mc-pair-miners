184de0f1530ff20bf19ea55c8d585db47b997c0a
default <none>
2013-09-15 20:43:30 +1000

    New lead and follow scripts save state and turn at the end of a line.

--- follow.lua
+++ follow.lua
@@ -1,39 +1,82 @@
 function mine_forward(distance)
-  if not distance then
-    error("Usage: script_name distance")
-  end
-  print("Starting tess for "..distance.." block")
+    if not distance then
+        error("Usage: script_name distance")
+    end
+    print("Starting tess for "..distance.." block")
 
-  turtle.select(16)
-  turtle.refuel()
-  if turtle.getFuelLevel() <= distance*1 then
-    error("Insufficient fuel to mine "..distance.." blocks", 0)
-  end
-
-  for i=1,distance do
-    triple_handshake('front')
-
-    while turtle.detect() do
-      sleep(0.1)
+    turtle.select(16)
+    turtle.refuel()
+    if turtle.getFuelLevel() <= distance*1 then
+        error("Insufficient fuel to mine "..distance.." blocks", 0)
     end
 
-    turtle.select(1)
-    turtle.place()
-    turtle.select(2)
-    turtle.placeDown()
-    sleep(5)
+    for i=1,distance do
+        triple_handshake('front')
 
-    turtle.digDown()
-    turtle.select(1)
-    turtle.dig()
+        while turtle.detect() do
+            sleep(0.1)
+        end
 
-    triple_handshake('front')
-    while turtle.detect() do
-      sleep(0.1)
+        turtle.select(1)
+        turtle.place()
+        turtle.select(2)
+        turtle.placeDown()
+        sleep(5)
+
+        turtle.digDown()
+        turtle.select(1)
+        turtle.dig()
+
+        triple_handshake('front')
+        while turtle.detect() do
+            sleep(0.1)
+        end
+        turtle.forward()
     end
-    turtle.forward()
-  end
 end
 
+function turn(direction, count)
+    for i=1,count do
+        while turtle.detect() do
+            sleep(0.5)
+        end
+        turtle.forward()
+        if direction == 'l' then
+            turtle.turnLeft()
+        elseif direction == 'r' then
+            turtle.turnRight()
+        else
+            return false
+        end
+    end
+end
+
+--- Mainline
 args = {...}
+
+if fs.exists("_state") then
+    print("From file")
+    f = fs.open("_state", "r")
+    state = textutils.unserialize(f.readAll())
+    f.close()
+else
+    state = {}
+    state["role"] = "follow"
+    state["distance"] = args[1]
+    state["progress"] = 0
+    state["zoffset"] = 0
+    state["nextturn"] = 'r'
+end
+
 mine_forward(args[1])
+
+turn(state["nextturn"], 2)
+if state["nextturn"] == 'r' then
+    state["nextturn"] = 'l'
+else
+    state["nextturn"] = 'r'
+end
+
+f = fs.open("_state", "w")
+f.write(textutils.serialize(state))
+f.close()
--- lead.lua
+++ lead.lua
@@ -1,38 +1,80 @@
 function mine_forward(distance)
-  if not distance then
-    error("Usage: script_name distance")
-  end
-  print("Starting miner for "..distance.." block")
+    if not distance then
+        error("Usage: script_name distance")
+    end
+    print("Starting miner for "..distance.." block")
 
-  turtle.select(16)
-  turtle.refuel()
-  if turtle.getFuelLevel() <= distance*3 then
-    error("Insufficient fuel to mine "..distance.." blocks", 0)
-  end
-
-  for i=1,distance do
-    triple_handshake('back')
-
-    turtle.digUp()
-    turtle.digDown()
-    turtle.select(1)
-    turtle.placeDown()
-    turtle.up()
-
-    sleep(5)
-
-    while turtle.detectDown() do
-      sleep(0.1)
+    turtle.select(16)
+    turtle.refuel()
+    if turtle.getFuelLevel() <= distance*3 then
+        error("Insufficient fuel to mine "..distance.." blocks", 0)
     end
 
-    turtle.down()
-    turtle.digDown()
+    for i=1,distance do
+        triple_handshake('back')
 
-    triple_handshake('back')
-    turtle.dig()
-    turtle.forward()
-  end
+        turtle.digUp()
+        turtle.digDown()
+        turtle.select(1)
+        turtle.placeDown()
+        turtle.up()
+
+        sleep(5)
+
+        while turtle.detectDown() do
+            sleep(0.1)
+        end
+
+        turtle.down()
+        turtle.digDown()
+
+        triple_handshake('back')
+        turtle.dig()
+        turtle.forward()
+    end
 end
 
+function turn(direction, count)
+    for i=1,count do
+        if direction == 'l' then
+            turtle.turnLeft()
+        elseif direction == 'r' then
+            turtle.turnRight()
+        else
+            return false
+        end
+        turtle.dig()
+        turtle.forward()
+        turtle.digDown()
+    end
+end
+
+-- Mainline
 args = {...}
+
+if fs.exists("_state") then
+    print("From file")
+    f = fs.open("_state", "r")
+    state = textutils.unserialize(f.readAll())
+    f.close()
+else
+    state = {}
+    state["role"] = "follow"
+    state["distance"] = args[1]
+    state["progress"] = 0
+    state["zoffset"] = 0
+    state["nextturn"] = 'r'
+end
+
 mine_forward(args[1])
+
+turn(state["nextturn"], 2)
+if state["nextturn"] == 'r' then
+    state["nextturn"] = 'l'
+else
+    state["nextturn"] = 'r'
+end
+
+f = fs.open("_state", "w")
+f.write(textutils.serialize(state))
+f.close()
