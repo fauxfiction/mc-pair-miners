f223b79e3f5f26792280e6781c384b41de26cde2
Matt <github@fauxfiction.com>
2013-09-16 23:02:01 +1000

    Handshake timeout and limited recovey.

--- redstone_comms.lua
+++ redstone_comms.lua
@@ -5,25 +5,45 @@ function redstone_pulse(side, type)
     redstone.setAnalogOutput(side, 0)
 end
 
+function redstone_timeout()
+    sleep(5)
+    return nil
+end
+
+function redstone_pullevent()
+    return os.pullEvent('redstone')
+end
+
 function redstone_listen(side)
     print("Waiting for redstone on ".. side)
-    os.pullEvent('redstone')
-    return REDSTONE[redstone.getAnalogInput(side)]
+    e = parallel.waitForAny(redstone_pullevent, redstone_timeout)
+    if e == 1 then -- waitForAny currently returns the arg number of the function that returned
+        return REDSTONE[redstone.getAnalogInput(side)]
+    else
+        if side == "front" then
+            print("Trying to move forward")
+            if not turtle.detect() then
+                turtle.forward()
+                return nil
+            end
+        end
+    end
 end
 
 function triple_handshake(side)
     redstone_pulse(side, "SYN")
-    while true do
+    for i=1,5 do
         response = redstone_listen(side)
         if response == "SYN" then
             redstone_pulse(side, "SYNACK")
         elseif response == "SYNACK" then
             redstone_pulse(side, "ACK")
-            break
+            return true
         elseif response == "ACK" then
-            break
+            return true
         else
             redstone_pulse(side, "SYN")
         end
     end
+    return false
 end
